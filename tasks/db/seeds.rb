ddbabcc3553187cd6dbeaab416a44f797e484e02
Devon W <devon.dana@gmail.com>
2024-05-04 15:08:17 -0400

    Create database seeders

--- tasks/db/seeds.rb
+++ tasks/db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+user = User.create(name: "Devon", email: "devon@example.com")
+
+user.tasks.create([
+    { name: "Wash car", description: "Go to the car wash", completed: false },
+    { name: "Sign up for course", description: "Sign up for the Motorcycles Safety Course", completed: false },
+    { name: "Submit assesment", description: "Submit the Savvy Test assesment", completed: true },
+])
